3c82f80de16b4db97cd35de6d7298b130154cc28
SGauthier2Pro <99419487+SGauthier2Pro@users.noreply.github.com>
2023-03-21 15:32:43 +0100

    Delete test_index.py
    RAZ master branch

--- tests/unit_tests/test_index.py
+++
@@ -1,9 +0,0 @@
-import tests.conftest
-from tests.conftest import client, mock_clubs, mock_competitions
-import server
-
-
-class TestIndex:
-    def test_index_route_should_status_code_ok(self, client):
-        response = client.get('/')
-        assert response.status_code == 200
